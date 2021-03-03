9682a3ff5e45385fb0ad3450975d9d9be81655b8
NTNN N <garamdang@gmail.com>
2021-03-03 17:08:50 +0900

    \n 문자열 정규표현식 구현해야함

--- 1_Crawl/dart_report.py
+++ 1_Crawl/dart_report.py
@@ -1,53 +1,108 @@
 import requests as rq
 import urllib as url
 
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import lxml
+#from jsonfinder import jsonfinder
 
-def webdriver(lnk, **kwarg):
-    # parse and find
-    # parse param & find param
-    # kwarg 확인
+import json
+import re
 
+import os
+
+def wd(lnk, types='', params={}, unwanted_params={}):
+    # wd stands for webdriver. Parse and Find
     # func(**{'type':'Event'}) is equivalent to func(type='Event')
-
-
-    # arg_ = {'types':'', 'params':{'':''}}
-    arg_ = {}
-    arg_keys = ['types', 'params',]
-    arg_vals = ['',{},]
-    # params
-    for k,v in kwarg.items():
-        print(k, v)
-        # 내가 원하는 키를 썼다면 value에 넣는다
-        if k in arg_keys:
-            arg_[k] = {v}
-            #arg_vals[]
-    print(arg_)
+    # unwanted_params 기능 추가
 
     # 옵션값 설정
     opt = webdriver.ChromeOptions()
     opt.add_argument('headless')
 
-    # 웹드라이버를 통해 네이버 금융 ETF 페이지에 접속
+    # 웹드라이버로 lnk parse
     drv = webdriver.Chrome('_exec/chromedriver.exe', options=opt)
     drv.implicitly_wait(3)
     try:
         drv.get(lnk)
-        
         # 뷰티풀 수프로 테이블을 스크래핑
         bs_result = bs(drv.page_source, 'lxml')
         drv.quit()
-        print(bs_result)
+        #print(types, params)
+        
+        tbs = bs_result.find_all(types, attrs=params) 
+        print(len(tbs))
+        for tb_i, tb in enumerate(tbs):
+            print(tb_i, tb.attrs)
+        return tbs
 
-        find_all_dict = {'class':'type_1',}
-        tbs = bs_result.find_all('table', attrs=find_all_dict)      # 각 api 설명 페이지의 주소를 정리해 놓은 테이블을 찾아
         #print(type(tb), tb)                                                    # 반환 결과가 있으면
-        for tb in tbs:
-            df = pd.read_html(str(tb))                 # dataFrame 형태로 받아옴.
-            pg.show(df)
+        #for tb in tbs:
+        #    str(tb).find()
+        #    #df = pd.read_html(str(tb))                 # dataFrame 형태로 받아옴.
+        #    #pg.show(df)
 
     except Exception as ex:
         print(ex)
 
-lnk = 'dart.fss.or.kr/dsaf001/main.do?rcpNo=20200330004441'
-parse_params={'script','text/javascript'}
-webdriver(lnk, types='script', params={'type':'text/javascript'})
+def bs_find(bs, start='', end=''):
+    # <!-- x-xeries js libraries  -->
+    # </script>
+    ret_list = []
+    try:
+        for b in bs:
+            if len(b) != 0:
+                st_idx = str(b).find(start)
+                ed_idx = str(b).find(end, st_idx)
+                #print(str(b)[st_idx:ed_idx])
+                ret_list.append(str(b)[st_idx:ed_idx])
+        return ret_list
+    except Exception as ex:
+        print(ex)
+
+def logfile_init(file=''):
+    try:
+        f = open(file, 'w')
+        return f
+    except Exception as ex:
+        print(ex)
+
+def logfile(f, file='', str_write=''):
+    f = open(file, 'a')
+    f.write(str_write)
+
+def __main__():
+    lnk = 'http://dart.fss.or.kr/dsaf001/main.do?rcpNo=20200330004441'
+    lnk_param = url.parse.urlparse(lnk)
+    parsekey=['scheme','netloc','path','params','query','fragment',]
+
+    #ParseResult(   scheme='http', 
+    #               netloc='dart.fss.or.kr', 
+    #               path='/dsaf001/main.do', 
+    #               params='', 
+    #               query='rcpNo=20200330004441', 
+    #               fragment='')
+
+    parse_params={'type':'text/javascript'}
+    tbs = wd(lnk, types='script', params = parse_params)
+
+    str_a = ['function initPage()','var viewport = new Ext.Viewport']#'//팝업 순서']
+    scr_list = bs_find(tbs, str_a[0], str_a[1])
+    print(len(scr_list))
+    [m.start() for m in re.finditer('([{](.*?)[}])', scr_list[0])]
+    #[m.start() for m in re.finditer('test', 'test test test test')]
+    for m in re.finditer('(text: ")(.*?)(",)', scr_list[0]):
+        print(m.span(), m.group())
+    for m in re.finditer('(viewDoc\(\')(.*?)(\'\))', scr_list[0]):
+        print(m.span(), m.group())
+        for n in re.finditer('(\').*?(\')', m.group()):
+            print(n.span(), n.group())
+    # http://dart.fss.or.kr/report/viewer.do?
+    # rcpNo=20200330004441&
+    # dcmNo=7206202&
+    # eleId=11&
+    # offset=349311&
+    # length=1264522&
+    # dtd=dart3.xsd
+if __name__ == "__main__":
+    __main__()

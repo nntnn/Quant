95281b50cfd400b2efdba2d01a838d97e0718df0
NTNN N <garamdang@gmail.com>
2021-03-09 00:24:00 +0900

    pickle로 dict 저장

--- 1_Crawl/dart_report.py
+++ 1_Crawl/dart_report.py
@@ -4,12 +4,19 @@ import urllib as url
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import lxml
-#from jsonfinder import jsonfinder
 
 import json
 import re
-
 import os
+
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+import pandas as pd
+import pandasgui as pg
+import pickle
 
 def wd(lnk, types='', params={}, unwanted_params={}):
     # wd stands for webdriver. Parse and Find
@@ -60,17 +67,6 @@ def bs_find(bs, start='', end=''):
     except Exception as ex:
         print(ex)
 
-def logfile_init(file=''):
-    try:
-        f = open(file, 'w')
-        return f
-    except Exception as ex:
-        print(ex)
-
-def logfile(f, file='', str_write=''):
-    f = open(file, 'a')
-    f.write(str_write)
-
 def __main__():
     lnk = 'http://dart.fss.or.kr/dsaf001/main.do?rcpNo=20200330004441'
     lnk_param = url.parse.urlparse(lnk)
@@ -86,7 +82,7 @@ def __main__():
     parse_params={'type':'text/javascript'}
     tbs = wd(lnk, types='script', params = parse_params)
 
-    str_a = ['function initPage()','function replaceHtml(']#'var viewport = new Ext.Viewport']#'//팝업 순서']
+    str_a = ['// 1', 'function replaceHtml(']   #function initPage()    'var viewport = new Ext.Viewport']#'//팝업 순서']
     scr_list = bs_find(tbs, str_a[0], str_a[1])
     print(len(scr_list))
     
@@ -94,9 +90,8 @@ def __main__():
     #[m.start() for m in re.finditer('test', 'test test test test')]
     for m in re.finditer('(text: ")(.*?)(",)', scr_list[0]):
         #print(m.span(), m.group())
-        txtlist.append(m.group())
-    print(len(txtlist))
-    print(txtlist)
+        txtlist.append(m.group()[m.group().find('text: "') + 7 : len(m.group()) - 2])
+    print(len(txtlist), txtlist)
 
     idxdict = {}
     for m_idx, m in enumerate(re.finditer('(viewDoc\(\')(.*?)(\'\))', scr_list[0])):
@@ -105,9 +100,30 @@ def __main__():
         for n in re.finditer('(\').*?(\')|(null)', m.group()):
             #print(n.span(), n.group())
             doclist.append(n.group())
-        idxdict[doclist[2]] = [doclist[3], doclist[4], doclist[5]] # txtlist[m_idx], 
-    print(len(idxdict))
-    print(idxdict)
+
+        for i_lst, lst in enumerate(doclist):
+            doclist[i_lst] = lst.replace('\'','')
+        try:
+            idxdict[doclist[2]] = [txtlist[m_idx], doclist[3], doclist[4], doclist[5]] # txtlist[m_idx], 
+        except:
+            pass
+        #print(m_idx, txtlist[m_idx])
+    print(len(idxdict), idxdict)
+
+    filename = '20200330004441.dump'
+    with open(filename,'wb') as fw:
+        pickle.dump(idxdict, fw)
+
+    with open(filename, 'rb') as handle:
+        b = pickle.load(handle)
+    
+    for k,v in b.items():
+        print(k, end='\t')
+        for vi in v:
+            print(vi, end='\t')
+        print('')
+    #print(b)
+
     for D_idx, DocValues in enumerate(re.finditer('(currentDocValues = {)(.*?)(};)', scr_list[0])):
         print(DocValues.span(), DocValues.group())
         curValues = DocValues.group()
@@ -115,7 +131,7 @@ def __main__():
             break
     curValues = curValues[curValues.find('{')+1:curValues.find('}')-1]
     Values = curValues.split(',')
-    print(Values)
+    #print(Values)
 
     # http://dart.fss.or.kr/report/viewer.do?
     # rcpNo=20200330004441&
@@ -124,5 +140,6 @@ def __main__():
     # offset=349311&
     # length=1264522&
     # dtd=dart3.xsd
+
 if __name__ == "__main__":
     __main__()
